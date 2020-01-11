bda710ab676119034079052226e831ebf11d4275
D1plo1d <thatotherdude@gmail.com>
2020-01-11 13:23:15 -0500

    fix: (regression) bind to any ip address

--- src/hyper_server.rs
+++ src/hyper_server.rs
@@ -231,7 +231,7 @@ pub async fn run(pool: PgPool) -> Result<(), Box<dyn std::error::Error + Send + 
         .parse()
         .expect("Invalid $PORT");
 
-    let addr = ([127, 0, 0, 1], port).into();
+    let addr = ([0, 0, 0, 0], port).into();
 
     // let pool = Arc::new(pool);
     let root_node = Arc::new(Schema::new(Query, Mutation{}));
