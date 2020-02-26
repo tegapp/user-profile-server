16ba24da2febfb15fe723373a714170d2d8cb3ce
D1plo1d <thatotherdude@gmail.com>
2020-02-25 20:03:27 -0500

    chore: Experimenting with sqlx

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,67 @@ use context::Context;
 
 use auth::{ upsert_user };
 
+
+error_chain::error_chain! {
+    // // The type defined for this error. These are the conventional
+    // // and recommended names, but they can be arbitrarily chosen.
+    // //
+    // // It is also possible to leave this section out entirely, or
+    // // leave it empty, and these names will be used automatically.
+    // types {
+    //     Error, ErrorKind, ResultExt, Result;
+    // }
+
+    // // Without the `Result` wrapper:
+    // //
+    // // types {
+    // //     Error, ErrorKind, ResultExt;
+    // // }
+
+    // // Automatic conversions between this error chain and other
+    // // error chains. In this case, it will e.g. generate an
+    // // `ErrorKind` variant called `Another` which in turn contains
+    // // the `other_error::ErrorKind`, with conversions from
+    // // `other_error::Error`.
+    // //
+    // // Optionally, some attributes can be added to a variant.
+    // //
+    // // This section can be empty.
+    // links {
+    //     Another(other_error::Error, other_error::ErrorKind) #[cfg(unix)];
+    // }
+
+    // // Automatic conversions between this error chain and other
+    // // error types not defined by the `error_chain!`. These will be
+    // // wrapped in a new error with, in the first case, the
+    // // `ErrorKind::Fmt` variant. The description and cause will
+    // // forward to the description and cause of the original error.
+    // //
+    // // Optionally, some attributes can be added to a variant.
+    // //
+    // // This section can be empty.
+    // foreign_links {
+    //     Fmt(::std::fmt::Error);
+    //     Io(::std::io::Error) #[cfg(unix)];
+    // }
+
+    // // Define additional `ErrorKind` variants.  Define custom responses with the
+    // // `description` and `display` calls.
+    // errors {
+    //     InvalidToolchainName(t: String) {
+    //         description("invalid toolchain name")
+    //         display("invalid toolchain name: '{}'", t)
+    //     }
+
+    //     // You can also add commas after description/display.
+    //     // This may work better with some editor auto-indentation modes:
+    //     UnknownToolchainVersion(v: String) {
+    //         description("unknown toolchain version"), // note the ,
+    //         display("unknown toolchain version: '{}'", v), // trailing comma is allowed
+    //     }
+    // }
+}
+
 pub type PgPool = Arc<Pool<ConnectionManager<PgConnection>>>;
 pub type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
@@ -53,6 +114,27 @@ fn main() {
         .expect("Invalid $PORT");
 
     let pool = establish_db_connection();
+
+    let database_url = env::var("POSTGRESQL_ADDON_URI")
+        .expect("$POSTGRESQL_ADDON_URI must be set");
+
+    let sqlx_pool = futures::executor::block_on(async {
+        sqlx::PgPool::new(&database_url)
+            .await
+            .map(|p| Arc::new(p))
+            .expect("Could not connect to Postgres")
+    });
+
+    futures::executor::block_on(async {
+        let users = sqlx::query_as!(
+            user::User,
+            "SELECT * FROM users ORDER BY id",
+        )
+            .fetch_all(&mut sqlx_pool.acquire().await.unwrap())
+            .await.unwrap();
+
+        println!("!!!!IT WORKED!!!!! {:?}", users);
+    });
 
     let homepage = warp::path::end().map(|| {
         Response::builder()
